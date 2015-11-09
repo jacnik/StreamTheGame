359430b0fef68c9e6ee712d50b4195a9c5c0c9ab
jacnik <jacnik@bitbucket.org>
2015-11-09 19:21:32 +0100

    main calculation loop set up

--- app/src/main/java/com/example/jacek/streamthegame/GamePanel.java
+++ app/src/main/java/com/example/jacek/streamthegame/GamePanel.java
@@ -1,14 +1,64 @@
 package com.example.jacek.streamthegame;
 
 import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
  * Created by jacek on 11/9/2015.
  */
-public class GamePanel extends SurfaceView {
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
+
+    private MainThread thread;
 
     public GamePanel(Context context) {
         super(context);
+
+        // add the callback to the surfaceholder to intercept events
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+
+        // make game panel focusable so it can handle events
+        setFocusable(true);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        return false;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        // we can safely start the game loop
+        this.thread.setRunning(true);
+        this.thread.run();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        while(retry) {
+           try{
+               this.thread.setRunning(false);
+               thread.join();
+
+           } catch (InterruptedException e) {
+               e.printStackTrace(); // todo handle it nicely
+           }
+            retry = false;
+        }
+    }
+
+    public void update() {
+
     }
 }
---
+++ app/src/main/java/com/example/jacek/streamthegame/MainThread.java
@@ -0,0 +1,76 @@
+package com.example.jacek.streamthegame;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by jacek on 11/9/2015.
+ */
+public class MainThread extends Thread {
+    private final int FPS = 30;
+    private double averageFPS;
+    private SurfaceHolder surfaceHolder;
+    private GamePanel gamePanel;
+    private boolean running;
+    private static Canvas canvas;
+
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void run() {
+        long startTime;
+        long timeMillis;
+        long waitTime = 0;
+        long totalTime = 0;
+        int frameCount = 0;
+        long targetTime = 1000/FPS;
+
+        while(this.running) {
+            startTime = System.nanoTime();
+            canvas = null;
+
+            //try locking the canvas for pixel editing
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (this.surfaceHolder) {
+                    this.gamePanel.update();
+                    this.gamePanel.draw(canvas);
+                }
+            } catch (Exception e) {
+                // todo handle main loop exceptions
+            }
+            timeMillis = (System.nanoTime() - startTime)/1000000;
+            waitTime = targetTime - timeMillis;
+
+            try {
+                this.sleep(waitTime);
+            } catch (Exception e) {
+                // todo handle sleep exceptions
+            } finally {
+                if (canvas != null) {
+                    // todo try/catch?
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+
+            }
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+
+            if(frameCount == FPS) {
+                averageFPS = 1000/((totalTime/frameCount)/1000000);
+                frameCount = 0;
+                totalTime = 0;
+                System.out.print(averageFPS); //todo this doesn't print but its ok since its just a mock
+            }
+        }
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+}
