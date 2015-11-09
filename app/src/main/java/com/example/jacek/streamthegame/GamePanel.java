389026d3c9a4f376d74ed6cce9c5455e556cdce2
jacnik <jacnik@bitbucket.org>
2015-11-09 17:36:16 +0100

    GamePanel created

---
+++ app/src/main/java/com/example/jacek/streamthegame/GamePanel.java
@@ -0,0 +1,14 @@
+package com.example.jacek.streamthegame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Created by jacek on 11/9/2015.
+ */
+public class GamePanel extends SurfaceView {
+
+    public GamePanel(Context context) {
+        super(context);
+    }
+}
--- app/src/main/java/com/example/jacek/streamthegame/MainActivity.java
+++ app/src/main/java/com/example/jacek/streamthegame/MainActivity.java
@@ -1,31 +1,25 @@
 package com.example.jacek.streamthegame;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // set full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new GamePanel(this));
     }
 
     @Override
