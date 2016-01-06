1d1c8f5f74f815fd3b328877a1413477c863240e
jacnik <jacnik@bitbucket.org>
2016-01-06 01:02:22 +0100

    debug levels in level provider

--- app/src/main/java/com/example/jacek/streamthegame/LevelProvider.java
+++ app/src/main/java/com/example/jacek/streamthegame/LevelProvider.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 public class LevelProvider {
     public LevelDefinition getLevel(int level) {
         switch (level) {
+            case 0:
+                return this.debugLevel_2();
             case 1:
                 return defaultLevel();
                 //break;
@@ -25,6 +27,33 @@ public class LevelProvider {
         objects.add(new GameObjectDefinition(Sprite.short_pipe, 1, 3, 1));
         objects.add(new GameObjectDefinition(Sprite.short_pipe, 1, 5));
         objects.add(new GameObjectDefinition(Sprite.bend2, 5, 7));
+        return new LevelDefinition(nRows, nCols, objects);
+    }
+
+    // section of levels for debugging purposes
+    private LevelDefinition debugLevel_1() {
+        int nRows = 7;
+        int nCols = 10;
+        ArrayList<GameObjectDefinition> objects = new ArrayList<>();
+        objects.add(new GameObjectDefinition(Sprite.exit, 3, 1, 4));
+        objects.add(new GameObjectDefinition(Sprite.enter, 3, 8, 4));
+        objects.add(new GameObjectDefinition(Sprite.short_pipe, 1, 1));
+        objects.add(new GameObjectDefinition(Sprite.short_pipe, 1, 3, 1));
+        objects.add(new GameObjectDefinition(Sprite.short_pipe, 1, 5));
+        objects.add(new GameObjectDefinition(Sprite.bend2, 5, 7));
+        return new LevelDefinition(nRows, nCols, objects);
+    }
+
+    private LevelDefinition debugLevel_2() {
+        int nRows = 7;
+        int nCols = 10;
+        ArrayList<GameObjectDefinition> objects = new ArrayList<>();
+        objects.add(new GameObjectDefinition(Sprite.exit, 0, 5, 1));
+        objects.add(new GameObjectDefinition(Sprite.enter, 5, 5, 1));
+        objects.add(new GameObjectDefinition(Sprite.short_pipe, 1, 1));
+        objects.add(new GameObjectDefinition(Sprite.short_pipe, 1, 3, 1));
+        objects.add(new GameObjectDefinition(Sprite.short_pipe, 1, 5));
+        objects.add(new GameObjectDefinition(Sprite.exit, 5, 0));
         return new LevelDefinition(nRows, nCols, objects);
     }
 }
