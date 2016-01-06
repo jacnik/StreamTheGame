8399e9ed3f59b96d3e4cbceea5db81c812b115c5
jacnik <jacnik@bitbucket.org>
2016-01-06 21:26:16 +0100

    event interface added (for previous commit)

---
+++ app/src/main/java/com/example/jacek/streamthegame/AnimationFinishedListener.java
@@ -0,0 +1,10 @@
+package com.example.jacek.streamthegame;
+
+import java.util.EventObject;
+
+/**
+ * Created by jacek on 1/6/2016.
+ */
+public interface AnimationFinishedListener {
+    public void animationFinished(EventObject event);
+}
